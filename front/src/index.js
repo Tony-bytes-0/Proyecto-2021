8d32994a6bac2b44f851f902f80f1b6fb7bb0e18
Tony Gonzalez <lonson574@gmail.com>
2022-11-14 09:28:41 -0400

    este es un proyecto estable con las variables de redux

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,19 @@
 import App from './components/App';
-
 import { createRoot } from 'react-dom/client';
+
+//redux
+import store from './indexModles/store'
+import { Provider } from 'react-redux'
+
+
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+
+
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
+);
