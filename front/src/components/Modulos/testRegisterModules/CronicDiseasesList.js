166a21405e0d7151d7c10456b6a1db9d45c77a0c
Tony <lonson574@gmail.com>
2022-12-02 14:55:49 -0400

    cambios en los switches

--- front/src/components/Modulos/testRegisterModules/CronicDiseasesList.js
+++ front/src/components/Modulos/testRegisterModules/CronicDiseasesList.js
@@ -1,15 +1,20 @@
 import { Switch, FormGroup, FormControlLabel } from "@mui/material"
-import { cronicDiseases } from "../Independientes/staticValuesList"
+import { useState } from "react"
 
 export default function CronicDiseasesList(props){
+    const [hideCronic, toggleCronic] = useState(true)
+    const handleChange = (event) => { if (event.target.checked) { toggleCronic(false) } else { toggleCronic(true) } }//disable de las enfermedades cronicas
+
+    const [epilepsia, setEpilepsia] = useState(false)
+    const handleEpilepsia = () => {if(epilepsia){setEpilepsia(false)}  else{setEpilepsia(true)}}
     return<>
         <FormGroup orientation="horizontal">
-            <FormControlLabel control={<Switch />} label='Enfermedades Cronicas' />
-            <FormControlLabel disabled control={<Switch />} label={cronicDiseases[0]} />
-            <FormControlLabel disabled control={<Switch />} label={cronicDiseases[1]} />
-            <FormControlLabel disabled control={<Switch />} label={cronicDiseases[2]} />
-            <FormControlLabel disabled control={<Switch />} label={cronicDiseases[3]} />
-            <FormControlLabel disabled control={<Switch />} label={cronicDiseases[4]} />
+            <FormControlLabel control={<Switch />} onChange={handleChange} label='Enfermedades Cronicas' />
+            <FormControlLabel disabled={hideCronic}  control={<Switch />} onChangue={handleEpilepsia} label={'Epilepsia'} />
+            <FormControlLabel disabled={hideCronic}  control={<Switch />} label={'Cancer'} />
+            <FormControlLabel disabled={hideCronic} control={<Switch />} label={'Diabetes'} />
+            <FormControlLabel disabled={hideCronic} control={<Switch />} label={'Artritis'} />
+            <FormControlLabel disabled={hideCronic} control={<Switch />} label={'Asma'} />
         </FormGroup>
     </>
 }
